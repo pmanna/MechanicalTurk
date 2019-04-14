26b9e58209b1d6694eff7584debc110097f5ff1c
Paolo <paolo@paolomanna.com>
2019-04-14 13:19:39 +0100

    Added serial input

--- DeltaRobot.ino
+++ DeltaRobot.ino
@@ -1,18 +1,22 @@
-// -*- mode: C++ -*-
 
+#include <string.h>
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 #include "Kinematics.h"
 
-//#define DEBUG
+#define DEBUG
+//#define TEST_MOVES
 
-#define MAGNET_PIN  PA8
-
+const int MAGNET_PIN  = PA8;
+const float MAX_Z     = -120.0;
 const long stepsPerRevolution = 4076;
 const float stepsPerDegree   = (float)stepsPerRevolution / 360.0;
 const long zeroOffsetSteps  = (long)(stepsPerDegree * 20.0);
 
-int progress = 0;
+char inData[80];
+char *outData[4] = {NULL};
+byte idx = 0;
+bool motorsEnabled = true;
 
 // Define a stepper and the pins it will use
 // WARNING: it's IN1, IN2, IN4, IN3, due to the winding!
@@ -21,6 +25,7 @@ AccelStepper motor2(AccelStepper::FULL4WIRE,PA4,PA5,PA7,PA6);
 AccelStepper motor3(AccelStepper::FULL4WIRE,PB0,PB1,PB11,PB10);
 MultiStepper arm;
 
+#ifdef TEST_MOVES
 float samplePositions[6][3] = {
   {0.0,0.0,-200.0},
   {15.0,15.0,-260.0},
@@ -32,11 +37,16 @@ float samplePositions[6][3] = {
 
 bool magnetOn[6] = {false, true, true, true, true, false};
 
+int progress = 0;
+#endif
+
 void enableMotors()
 {
   motor1.enableOutputs();
   motor2.enableOutputs();
   motor3.enableOutputs();
+
+  motorsEnabled = true;
 }
 
 void disableMotors()
@@ -44,6 +54,8 @@ void disableMotors()
   motor1.disableOutputs();
   motor2.disableOutputs();
   motor3.disableOutputs();
+
+  motorsEnabled = false;
 }
 
 void resetMotors(long backstep)
@@ -78,16 +90,24 @@ void moveRobotTo(float positions[3]) {
   float angle1, angle2, angle3;
   long steps[3];
 
+  // Ensure arms aren't set to an invalid physical position
+  // (found empirically, more checks within the Kinematics)
+  if (positions[2] > MAX_Z) positions[2] = MAX_Z;
+  
+  if (!motorsEnabled) enableMotors();
+  
   if (delta_calcInverse(positions[0], positions[1], positions[2], angle1, angle2, angle3) == 0) {
     steps[0]  = (long)(angle1 * stepsPerDegree) + zeroOffsetSteps;
     steps[1]  = (long)(angle2 * stepsPerDegree) + zeroOffsetSteps;
     steps[2]  = (long)(angle3 * stepsPerDegree) + zeroOffsetSteps;
 
 #ifdef DEBUG
+    Serial.print("Angles: ");
     Serial.print(angle1); Serial.print("˚  ");
     Serial.print(angle2); Serial.print("˚  ");
     Serial.print(angle3); Serial.println("˚  ");
     
+    Serial.print("Steps:  ");
     Serial.print(steps[0]); Serial.print("  ");
     Serial.print(steps[1]); Serial.print("  ");
     Serial.println(steps[2]);
@@ -117,15 +137,89 @@ void setup()
   arm.addStepper(motor3);
 
   resetMotors(-30);
-
-  enableMotors();
+  
+#ifdef TEST_MOVES
   moveRobotTo(samplePositions[0]);
+#endif
+
+#ifdef DEBUG
+  Serial.println("Ready");
+#endif
 }
+
+#ifndef TEST_MOVES
+bool parseInput()
+{
+  while(Serial.available() > 0)
+  {
+    char aChar = Serial.read();
+    
+    if(aChar == ';')
+    {
+      char *p = inData; //point to *p to the string in inData
+      char *str;        //declaring *str
+      int counter = 0; //initialise the counter
+      
+      while (str = strtok(p, ",")) // delimiter is the comma
+      {
+        outData[counter] = str; //use the counter as an index to add each value to the array
+        counter++; //increment the counter
+        
+        p = NULL;
+        
+        if (counter > 3) break;
+      }
+      
+ #ifdef DEBUG
+     if (!strcmp(outData[0],"R")) {
+        Serial.println("Resetting");
+      } else {
+        Serial.print("X: "); Serial.println(atof(outData[0]));
+        Serial.print("Y: "); Serial.println(atof(outData[1]));
+        Serial.print("Z: "); Serial.println(atof(outData[2]));
+        Serial.print("M: "); Serial.println(atoi(outData[3]));
+      }
+ #endif
+     if (!strcmp(outData[0],"R")) {
+        resetMotors(-20);
+      } else {
+        float target[3];
+        bool magnetOn;
+        
+        target[0] = atof(outData[0]);
+        target[1] = atof(outData[1]);
+        target[2] = atof(outData[2]);
+        
+        moveRobotTo(target);
+
+        magnetOn = atoi(outData[3]) != 0;
+        if (magnetOn) {
+          digitalWrite(MAGNET_PIN, HIGH);
+        } else {
+          digitalWrite(MAGNET_PIN, LOW);
+        }
+        
+        delay(500);
+      }
+      
+      for (int ii=0; ii<4; ii++) {
+        outData[ii] = NULL;
+      }
+      
+      idx = 0;
+      inData[idx] = NULL;
+    } else if (idx < 80) {
+      inData[idx] = aChar;
+      idx++;
+      inData[idx] = '\0'; // Keep the string NULL terminated
+    }
+  }
+}
+#endif  // TEST_MOVES
 
 void loop()
 {
-  // If at the end of travel go to the other end
-  
+#ifdef TEST_MOVES
   if (!arm.run()) {
     progress++;
 
@@ -145,4 +239,9 @@ void loop()
 
     delay(500);
   }
+#else
+
+  if (!arm.run()) parseInput();
+  
+#endif
 }
